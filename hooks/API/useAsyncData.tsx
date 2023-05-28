1888d06941f3036f4889ccf77633676537f1efee
Ruslan <ruslanryscovbluerex@gmail.com>
2023-05-28 21:55:06 +0200

    add docs

--- hooks/API/useAsyncData.tsx
+++ hooks/API/useAsyncData.tsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from 'react';
 
 type UseAsyncDataResult<T> = [T | null, boolean, string];
-
+/** useAsyncData is a custom hook that returns data from an async request.
+ * @template R
+ * @param {() => Promise<R>} request - The async request.
+ * @returns {[R | null, boolean, string]} - A tuple containing the data, loading state and error message.
+ * @example
+ * const [data, loading, error] = useAsyncData(() => axios.get('https://jsonplaceholder.typicode.com/todos/1'));
+ * if (loading) return <p>Loading...</p>;
+ * if (error) return <p>{error}</p>;
+ * return <p>{data?.title}</p>;
+ * */
 const useAsyncData = <R,>(request: () => Promise<R>): UseAsyncDataResult<R> => {
   const [data, setData] = useState<R | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
@@ -13,6 +22,11 @@ const useAsyncData = <R,>(request: () => Promise<R>): UseAsyncDataResult<R> => {
       .then((response) => setData((response as { data: R }).data))
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
+    return () => {
+      setData(null);
+      setLoading(false);
+      setError('');
+    };
   }, []);
 
   return [data, loading, error];
